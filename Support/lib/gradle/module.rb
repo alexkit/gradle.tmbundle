4cede8de1d027904ea4bfa16e4a1c40c21747171
Luke Daley <ld@ldaley.com>
2011-06-22 16:44:48 +1000

    Improvements to testing single files.

--- Support/lib/gradle/module.rb
+++ Support/lib/gradle/module.rb
@@ -13,22 +13,38 @@ module Gradle
     def prefix_task(task) 
       @name.empty? ? task : "#{name}:#{task}"
     end
-  
-    def test_single_arg(file = ENV['TM_SELECTED_FILE']) 
+    
+    def test_single(file = ENV['TM_SELECTED_FILE'])
       if file.nil?
         puts "No file selection"
         exit 1
       end
-
-      clazz = File.basename(file, File.extname(file))
-      prefix = @name.empty? ? "" : "#{@name}."
-      "-D#{prefix}test.single=#{clazz}"
+      
+      task = test_task_for_file(file)
+      run(task, test_single_arg(file, task))
     end
-  
-    def test_single(file = ENV['TM_SELECTED_FILE'])
-      run("test", test_single_arg(file))
+    
+    def test_task_for_file(file = ENV['TM_SELECTED_FILE'])
+      calculate_source_root(file)
     end
-  
+    
+    def calculate_source_root(file = ENV['TM_SELECTED_FILE'])
+      module_relative = module_relative_path(file)
+      if module_relative =~ /src\/(\w+)\/.+/
+        $1
+      else
+        raise "cannot determine source root for ‘#{file}’ as it is not under the module relative path of ‘src/«root»’"
+      end
+    end
+    
+    def module_relative_path(file = ENV['TM_SELECTED_FILE'])
+      if file.start_with? @path
+        file.sub /^@path\//, ""
+      else
+        raise "cannot get module relative path of ‘#{file}’ as it is not a child of ‘#{@path}’"
+      end
+    end 
+    
     def prompt_for_command_and_run
       previous = @prefs.get("prev_prompt")
       command = TextMate::UI.request_string(
@@ -55,6 +71,15 @@ module Gradle
       prefixed_args = args.collect { |a| a[0..0] == "-" ? a : prefix_task(a) }
       @project.run(prefixed_args)
     end
+    
+    private
+    
+    def test_single_arg(file, task) 
+      clazz = File.basename(file, File.extname(file))
+      prefix = @name.empty? ? "" : ":#{@name}:"
+      "-D#{prefix}#{task}.single=#{clazz}"
+    end
+    
   end
   
 end
